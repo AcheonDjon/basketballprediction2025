87cc4a0d3a4020248991790044b08a8eb6c0b24e
BitProgramer <afu25400@gmail.com>
2025-02-26 21:55:52 -0500

    Fixed Inconsistent Separator Use

--- src/winColumn.py
+++ src/winColumn.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("data/merged_team_games.csv", sep=",")
+df = pd.read_csv("data/merged_team_games.csv", sep="\t")
 
 df["Win"] = (df["team_score"] > df["opponent_team_score"]).astype(int)
 df['Point_Differential'] = df["team_score"] - df["opponent_team_score"]
