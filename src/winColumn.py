493b9a6704fb0923b54c049a9ac4ba79348f2b1c
BitProgramer <afu25400@gmail.com>
2025-02-26 21:52:09 -0500

    Added Point Differential Column (Run this first)

--- src/winColumn.py
+++ src/winColumn.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("data/merged_team_games.csv", sep="\t")
+df = pd.read_csv("data/merged_team_games.csv", sep=",")
 
 df["Win"] = (df["team_score"] > df["opponent_team_score"]).astype(int)
-
+df['Point_Differential'] = df["team_score"] - df["opponent_team_score"]
 df.to_csv("data/merged_team_games.csv", sep="\t", index=False)
